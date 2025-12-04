f554f4285eedd9ad79c10f793fa6720453e6689d
Muhammad Salman Sarwar <muhammadsalmansarwar32@gmail.com>
2025-12-05 02:51:29 +0500

    upw

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import Solutions from "./pages/Solutions";
-import AboutUs from "./pages/AboutUs";
-import Pricing from "./pages/Pricing";
+// import Layout from "./components/Layout";
+// import Home from "./pages/Home";
+// import Solutions from "./pages/Solutions";
+// import AboutUs from "./pages/AboutUs";
+// import Pricing from "./pages/Pricing";
 import Upwork from "./pages/Upwork";
 
 export default function SchoolManagementLanding() {
